49fc428cedfe0156122c35c41e6d433b8e161736
sangkyun <sangkyun.kim15@gmail.com>
2024-02-07 19:42:00 -0800

    Edit OAuth2Redirect.js

--- src/components/home/OAuth2Redirect.js
+++ src/components/home/OAuth2Redirect.js
@@ -16,6 +16,7 @@ function OAuth2Redirect() {
       const redirect = '/'
       setRedirectTo(redirect)
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const extractUrlParameter = (key) => {
